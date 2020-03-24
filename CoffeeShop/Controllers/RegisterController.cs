bef1fd71bfdfa8e94dc71af1989eca65ded08137
Tom Barron <tombarron@quickenloans.com>
2020-03-24 17:37:17 -0400

    MVP Baby!

---
+++ CoffeeShop/Controllers/RegisterController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("RegisterIndex");
+        }
+        [HttpPost]
+        public IActionResult Register(string fname, string lname, string email, string password)
+        {
+            ViewData["name"] = fname + " " + lname;
+            ViewData["email"] = email;
+            ViewData["password"] = password;
+            return View();
+        }
+    }
+}
